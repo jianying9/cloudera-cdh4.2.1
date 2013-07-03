3027dd08a94bb9e13637316cdae99ebaef0920d2
aladdin <ljy18659199765@gmail.com>
2013-07-03 15:28:51 +0800

    1.add cloudera config 2.add word-count example

---
+++ word-count/src/main/java/com/cloudera/mapred/JobStart.java
@@ -0,0 +1,45 @@
+package com.cloudera.mapred;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.examples.WordCount;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class JobStart extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new JobStart(), args);
+        System.exit(res);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        int result;
+        Configuration conf = this.getConf();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            result = 2;
+        } else {
+            Job job = new Job(conf, "word count");
+            job.setJarByClass(WordCount.class);
+            job.setMapperClass(WordCount.TokenizerMapper.class);
+            job.setCombinerClass(WordCount.IntSumReducer.class);
+            job.setReducerClass(WordCount.IntSumReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+            FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+            FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+            result = job.waitForCompletion(true) ? 0 : 1;
+        }
+        return result;
+    }
+}
